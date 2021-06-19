4d9aad2435900f3ba204919776dc83595d512d65
Umut Karakulak <umutkarakulak@hotmail.com>
2021-06-19 21:44:55 +0900

    update rollup config to resolve node modules and export commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,20 @@
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
+
 export default {
   input: "src/javascript/index.js",
   output: {
     file: "dist/javascript/index.js",
     format: "cjs",
   },
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+      browser: true,
+    }),
+    commonjs({
+      include: "node_modules/**",
+    }),
+  ],
 };
